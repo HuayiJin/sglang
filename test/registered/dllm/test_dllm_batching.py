f11aa2bbbd189ec6000f8c914eef56cd451d9c1a
jinhuayi <keplar_gd@icloud.com>
2026-01-27 16:34:08 +0800

    update dllm fdfo with pre-commit

--- test/registered/dllm/test_dllm_batching.py
+++ test/registered/dllm/test_dllm_batching.py
@@ -38,6 +38,52 @@ class TestBatching(CustomTestCase):
             "flashinfer",
             "--dllm-algorithm",
             "LowConfidence",
+            # "LowConfidenceFDFO", # try this for 2-4 times speed up
+        ]
+
+        cls.process = popen_launch_server(
+            cls.model,
+            cls.base_url,
+            timeout=DEFAULT_TIMEOUT_FOR_SERVER_LAUNCH,
+            other_args=other_args,
+        )
+
+    @classmethod
+    def tearDownClass(cls):
+        kill_process_tree(cls.process.pid)
+
+    def test_gsm8k(self):
+        args = SimpleNamespace(
+            num_shots=5,
+            data_path=None,
+            num_questions=200,
+            max_new_tokens=512,
+            parallel=128,
+            host="http://127.0.0.1",
+            port=int(self.base_url.split(":")[-1]),
+        )
+        metrics = run_eval_few_shot_gsm8k(args)
+        print(f"{metrics=}")
+
+        self.assertGreater(metrics["accuracy"], 0.88)
+
+
+class TestBatchingFDFO(CustomTestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.model = "inclusionAI/LLaDA2.0-mini"
+        cls.base_url = DEFAULT_URL_FOR_TEST
+
+        other_args = [
+            "--trust-remote-code",
+            "--mem-fraction-static",
+            "0.9",
+            "--max-running-requests",
+            "4",
+            "--attention-backend",
+            "flashinfer",
+            "--dllm-algorithm",
+            "LowConfidenceFDFO",
         ]
 
         cls.process = popen_launch_server(
